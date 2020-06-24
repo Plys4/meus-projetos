d7270eb82095f1588e0728b19ad3a921778347b5
Vitor <vitorbraun0507@gmail.com>
2020-06-24 12:06:14 -0300

    add ubt_posint.sh

--- posinst_ubuntu.sh
+++ posinst_ubuntu.sh
@@ -3,8 +3,11 @@ sudo apt install zsh flatpak gnome-software-plugin-flatpak papirus-icon-theme -y
 wget -qO- https://git.io/papirus-folders-install | sh
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.axosoft.GitKraken -y
+sudo add-apt-repository ppa:lutris-team/lutris
+sudo apt-get update 
+sudo apt-get install lutris
+
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" -y
-# - - - #
 sed -i s/ZSH_THEME="robbyrussell"/ZSH_THEME="bira"/g .zshrc
 
 
