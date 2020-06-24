a4801ecd7b1a0ec5ab03ae9036bd9e0360700c26
Plys4 <57731017+Plys4@users.noreply.github.com>
2020-06-24 15:32:43 -0300

    Update posinst_ubuntu.sh

--- posinst_ubuntu.sh
+++ posinst_ubuntu.sh
@@ -2,7 +2,12 @@
 sudo apt install zsh flatpak gnome-software-plugin-flatpak papirus-icon-theme -y
 wget -qO- https://git.io/papirus-folders-install | sh
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.axosoft.GitKraken -y
+#flatpak install flathub com.axosoft.GitKraken -y
+
+sudo dpkg --add-architecture i386 
+wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main' 
+sudo apt install --install-recommends winehq-stable
 sudo add-apt-repository ppa:lutris-team/lutris
 sudo apt-get update 
 sudo apt-get install lutris
