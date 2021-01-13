6bc8fb388d756391aed3ecbc11f96024f14ee8ef
Plys4 <57731017+Plys4@users.noreply.github.com>
2021-01-13 00:30:48 -0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,54 +1,52 @@
-#/bin/bash
+#!/bin/bash
+
+sudo rm /var/lib/dpkg/lock-frontend
+
+sudo rm /var/cache/apt/archives/lock
+
+sudo dpkg --add-architecture i386
+
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' 
+
+sudo apt update
+
+sudo add-apt-repository ppa:lutris-team/lutris -y
+
+sudo apt update
+
+sudo apt install mesa-vulkan-drivers mesa-vulkan-drivers:i386 lutris libvulkan1 libvulkan1:i386 -y
+
+sudo apt-get install --install-recommends winehq-stable wine-stable wine-stable-i386 wine-stable-amd64 -y
+
+sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y
+
+cd Downloads/
+
+wget -c https://linux.dropbox.com/packages/ubuntu/dropbox_2020.03.04_amd64.deb
+
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+wget -c https://dl.discordapp.net/apps/linux/0.0.13/discord-0.0.13.deb
+
+cd $HOME
+
+sudo apt install flatpak flameshot steam-installer libvulkan1 mesa-vulkan-drivers mesa-vulkan-drivers:i386 libvulkan1:i386 libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 build-essential zsh -y
+
+sudo apt install flatpak -y
+      
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub com.github.calo001.fondo
+flatpak install flathub com.rafaelmardojai.Blanket
 
 
-## GERAL
-sudo dpkg --add-architecture i386 
-sudo apt install grub-theme-breeze grub-customizer apt-transport-https build-essential curl zsh git vim nano openjdk-8-jdk -y
-wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg
-add -
+sudo add-apt-repository ppa:ernstp/mesarc -y
+sudo apt-get update
+sudo apt install corectrl -y
 
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" -y
 
-## VARIAVEIS
-DOWNLOAD_CHROME=https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-DOWNLOAD_DROPBOX=https://linux.dropbox.com/packages/ubuntu/dropbox_2020.03.04_amd64.deb
-
-
-## REPOSITORIOS DE TERCEIROS
-    echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list 
-    sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y
-    sudo add-apt-repository ppa:papirus/papirus -y
-    sudo add-apt-repository ppa:lutris-team/lutris -y
-    sudo apt update
-
-
-## APLICATIVOS DOS REPOSITORIOS DE TERCEIROS
-sudo apt-get install papirus-icon-theme papirus-folders -y
-sudo apt install brave-browser -y
-sudo apt install --install-recommends winehq-stable -y
-sudo apt-get install lutris -y 
-
-
-## SPOTIFY
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
-
-
-## OH MY ZSH
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    sed -i 's:ZSH_THEME="robbyrussell":ZSH_THEME="bira":g' .zshrc
-cd
-
-
-## aplicativos .deb
-mkdir .instalacao
-    cd .instalacao
-        wget -c $DOWNLOAD_CHROME 
-        wget -c $DOWNLOAD_DROPBOX
-            sudo dpkg -i *.deb
-            
-            
-cd
-
-sudo apt update ; sudo apt dist-upgrade
+sudo apt update ; sudo apt dist-upgrade -y
